37012e9664a79d9d1dd3069d7daee7275f66b8da
S <s>
2023-10-12 17:24:51 +0400

    {"types":"C","runs":1}

--- Problem12.java
+++ Problem12.java
@@ -3,22 +3,17 @@ import acm.program.*;
 
 public class Problem12 extends GraphicsProgram{
 	public void run() {
-		drawWall();
-		drawDoor();
-		//drawWindow();
-		//drawRoof();
+		draw();
 		
 	}
-
-	private void drawDoor() {
-		GRect Door
-		
-	}
-
-	private void drawWall() {
-		GRect Wall = new GRect(500, 500);
-		add(Wall, 400, 400);
-		
+	
+	private void draw() {
+		double x1 = 0;
+		double x2 = getWidth();
+		double y1 = getHeight() / 2;
+		double y2 = y1;
+		GLine line = new GLine(x1, y1, x2, y2);
+		add(line);
 	}
 
 }

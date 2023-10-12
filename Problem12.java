0865d516f8ebeb2b0797491a055b9e9e39749b58
S <s>
2023-10-12 17:38:53 +0400

    {"types":"C","runs":2}

--- Problem12.java
+++ Problem12.java
@@ -2,12 +2,38 @@ import acm.graphics.*;
 import acm.program.*;
 
 public class Problem12 extends GraphicsProgram{
+	
+//	private static final double PI = 3.14;
+	
+	private static final double HOUSE_WIDTH = 200;
+	private static final double HOUSE_HEIGHT = 200;
+	private static final double DOOR_WIDTH = 40;
+	private static final double DOOR_HEIGHT = 60;
+	
+	
 	public void run() {
-		draw();
+		drawCentreLine();
+		drawHouse();
 		
 	}
 	
-	private void draw() {
+	private void drawHouse() {
+		drawWall();
+		drawRoof();
+		draw
+		
+	}
+
+	private void drawWall() {
+		GRect wall = new GRect(HOUSE_WIDTH, HOUSE_HEIGHT);
+		int x = (int) (getWidth() / 2 - HOUSE_WIDTH / 2);
+		double y = getHeight() - HOUSE_HEIGHT; 
+		add(wall, x, y);
+	
+		
+	}
+
+	private void drawCentreLine() {
 		double x1 = 0;
 		double x2 = getWidth();
 		double y1 = getHeight() / 2;

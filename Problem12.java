39fae3638fd83aeee348b3b4a7a87d209b563913
S <s>
2023-10-12 17:49:08 +0400

    {"types":"C","runs":5}

--- Problem12.java
+++ Problem12.java
@@ -10,7 +10,7 @@ public class Problem12 extends GraphicsProgram{
 	private static final double DOOR_WIDTH = 40;
 	private static final double DOOR_HEIGHT = 60;
 	private static final double ROOF_HEIGHT = 100;
-	private static final double ROOF_WIDTH = 60;
+	
 	
 	
 	public void run() {
@@ -21,9 +21,32 @@ public class Problem12 extends GraphicsProgram{
 	
 	private void drawHouse() {
 		drawWall();
-//		drawRoof();
+		drawRoof();
 //		draw
 		
+	}
+
+	private void drawRoof() {
+		double x1 = (int) (getWidth() / 2 - HOUSE_WIDTH / 2);
+		double x2 = x1 + HOUSE_WIDTH / 2;
+		double y1 = getHeight() - HOUSE_HEIGHT; 
+		double y2 = y1 - ROOF_HEIGHT;
+		
+		for (int i = 0; i < 2; i++) {
+			if (i == 0) {
+				
+				
+			} else {
+				x1 = x2;
+				y1 = y2;
+				x2 = x1 + HOUSE_WIDTH / 2;
+				y2 = y1 + ROOF_HEIGHT /2;
+					
+			}
+		
+		GLine leftRoof = new GLine(x1, y1, x2, y2);
+		add(leftRoof);
+		}
 	}
 
 	private void drawWall() {

4f9132fe813c26d59ee2689bf6a2c2740a2a55af
S <s>
2023-10-14 12:03:14 +0400

    {"types":"C","runs":1}

--- Assignment01.java
+++ Assignment01.java
@@ -1,4 +1,38 @@
+import stanford.karel.SuperKarel;
 
-public class Assignment01 {
+public class Assignment01 extends SuperKarel {
+	public void run() {
+		moveToMagazine();
+		pickMagazine();
+		returnToInitialPosition();
+	}
+
+	private void returnToInitialPosition() {
+		turnAround();
+		for( int i = 0; i < 3; i++) {
+			move();
+		}
+		turnRight();
+		move();
+		turnRight();
+		
+	}
+
+	private void pickMagazine() {
+		pickBeeper();
+		
+	}
+
+	private void moveToMagazine() {
+		move();
+		move();
+		turnRight();
+		move();
+		turnLeft();
+		move();
+		turnRight();
+		move();
+		
+	}
 
 }

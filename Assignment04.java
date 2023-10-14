dfa799600205b6d79dde7f4424865e9c8498fc94
S <s>
2023-10-14 13:54:10 +0400

    {"types":"C","runs":13}

--- Assignment04.java
+++ Assignment04.java
@@ -1,4 +1,40 @@
+import stanford.karel.SuperKarel;
 
-public class Assignment04 {
+public class Assignment04 extends SuperKarel {
+	public void run() {
+		fillTheLine();
+		moveAllBeepersToStart();
+	}
+
+	private void moveBeeperToStart() {
+		pickBeeper();
+		turnAround();
+		while(frontIsClear()) {
+			move();
+		}
+		putBeeper();
+		turnAround();
+	}
+
+	
+	
+	private void moveAllBeepersToStart() {
+		while(frontIsClear()) {
+			move();
+			if(beepersPresent()) {
+				moveBeeperToStart();
+			}
+		}
+		
+	}
+
+	private void fillTheLine() {
+		while(frontIsClear()) {
+			putBeeper();
+			move();
+		}
+		putBeeper();
+	}
+	
 
 }

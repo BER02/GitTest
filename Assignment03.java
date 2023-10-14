d2bc912b647110f508216810b30a968f16be61d9
S <s>
2023-10-14 12:59:15 +0400

    {"types":"C","runs":8}

--- Assignment03.java
+++ Assignment03.java
@@ -2,31 +2,44 @@ import stanford.karel.SuperKarel;
 
 public class Assignment03 extends SuperKarel{
 	public void run() {
-		fillTwoRows();
+		fillOddRow();
+		turnRight();
+		move();
+		turnRight();
+		fillEvenRow();
 	}
 
-	private void fillTwoRows() {
+	private void fillEvenRow() {
+		while(frontIsClear()) {
+			move();
+			if(frontIsClear()) {
+				move();
+				putBeeper();
+			}
+		}
+		returnToStartOfRow();
+		
+	}
+
+	private void returnToStartOfRow() {
+		turnAround();
+		while(frontIsClear()) {
+			move();
+		}
+		
+	}
+
+	private void fillOddRow() {
 		putBeeper();
 		while(frontIsClear()) {
 			move();
 			if(frontIsClear()) {
 				move();
-				putBeeper();	
+				putBeeper();
 			}
 		}
-		turnLeft();
-		move();
-		turnLeft();
-		while(frontIsClear()) {
-			move();
-			if(frontIsClear()) {
-				move();
-				putBeeper();	
-	        }
-        }
-		turnRight();
-		move();
-		turnRight();
+		returnToStartOfRow();
 	}
 	
 }
+

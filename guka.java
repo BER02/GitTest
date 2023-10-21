e867f121255f5e80e35c38400eb059b1bf98176b
S <s>
2023-10-21 13:44:35 +0400

    {"types":"C","runs":1}

--- guka.java
+++ guka.java
@@ -1,9 +1,11 @@
 import acm.graphics.GLabel;
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class guka extends GraphicsProgram {
 	public void run() {
 		 add(new GLabel("Hello, world, from guka!", 100,75));
+		 add(new GRect(100, 100, 100, 100));
 	}
 	
 }

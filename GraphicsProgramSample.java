b56a3ac8d63ea0222df58b6e4ec4c60b55ad072a
S <s>
2023-10-26 13:09:41 +0400

    {"types":"C","runs":6}

--- GraphicsProgramSample.java
+++ GraphicsProgramSample.java
@@ -16,6 +16,8 @@ public class GraphicsProgramSample extends GraphicsProgram {
 		add(rect, 40, 40); // placement
 		add(new GOval(100, 100, 500, 500)); // adds new figure (type, x, y, width, height)
 		add(new GLabel("BERO" , 40, 4)); // adds new text x, y
+		GOval line = new GOval(100, 15, 100, 200); // x, y, width, height
+		add(line);
 	}
 
 }

3a172bbe9ab898c175c5f0bc04d492129a30a79c
S <s>
2023-10-26 14:16:30 +0400

    {"types":"C","runs":16}

--- ConsoleProgrammSample.java
+++ ConsoleProgrammSample.java
@@ -11,13 +11,16 @@ public class ConsoleProgrammSample extends ConsoleProgram {
 
 	public void run() {
 		println("This program adds two integers."); // prints line
-		int n1 = readInt("Enter n1: "); // reads ints
+		int n1 = readInt("Enter n1: "); // reads ints + text
 		int n2 = readInt("Enter n2: ");
-		int total = n1 + n2; // sum of two int
-		println("The total is " + total + "."); // prints sum
+		int total = n1 + n2; // sum of two int 
+		println("The total is " + total + "."); // prints sum + text
 		int n3 = readInt();
 		int total1 = n1 + n2 + n3;
 		println(total1);
+		double n4 = readDouble();
+		double total2 = n1 + n2 + n3 + n4;
+		println(total2);
 	}
 
 }

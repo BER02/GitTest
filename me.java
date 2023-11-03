a1e739f04a898e74153e8ebb145f2e400b780170
S <s>
2023-11-03 16:56:53 +0400

    {"types":"C","runs":1}

--- me.java
+++ me.java
@@ -1,4 +1,14 @@
-
-public class me {
-
+import acm.program.*;
+public class me extends ConsoleProgram {
+ public void run() {
+ println("This program converts integers to card ranks.");
+ int n = readInt("Enter an integer between 1 and 13: ");
+ switch (n) {
+ case 1: println("Ace"); break;
+ case 11: println("Jack"); break;
+ case 12: println("Queen"); break;
+ case 13: println("King"); break;
+ default: println(n); break;
+ }
+ }
 }

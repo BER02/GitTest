323979cb91df591e80419ecbacd2810ef541ab03
S <s>
2023-11-04 12:49:14 +0400

    {"types":"C","runs":7}

--- Problem13.java
+++ Problem13.java
@@ -8,14 +8,14 @@ public class Problem13 extends GraphicsProgram{
 
 	private void drawMidLine() {
 		double x1 = 0;
-		double y1 = getHeight() / 2;
-		double x2 = getWidth();
+		double y1 = (int) getHeight() / 2;
+		double x2 = (int) getWidth();
 		double y2 = y1;
 		GLine midLine = new GLine(x1, y1, x2, y2);
-		//add(midLine);
+		add(midLine);
 		
 		double y3 = (double) getHeight() / 2;
-		add(new GLine(x1, y3, x2, y3));
+		//add(new GLine(x1, y3, x2, y3));
 	}
 	
 

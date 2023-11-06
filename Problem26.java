d2a89e00bf5aaaf030681ef1df088aca0ef92611
S <s>
2023-11-06 12:04:12 +0400

    {"types":"C","runs":1}

--- Problem26.java
+++ Problem26.java
@@ -1,4 +1,17 @@
+import acm.program.ConsoleProgram;
 
-public class Problem26 {
+public class Problem26 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("Enter n: " );
+		int m = readInt("Enter m: ");
+		
+		println(GCD(n, m));
+	}
 
+	private int GCD(int x, int y) {
+		for (int i = Math.min(x, y); i >= 1; i--) {
+			if(x % i == 0 && y % i == 1) return i;
+		}
+		
+	}
 }

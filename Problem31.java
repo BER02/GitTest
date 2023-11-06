cc16403662da9210696fd15e900b05f8e0de9568
S <s>
2023-11-06 15:42:51 +0400

    {"types":"C","runs":30}

--- Problem31.java
+++ Problem31.java
@@ -11,7 +11,7 @@ public class Problem31 extends ConsoleProgram{
 		
 		if (n == 1) return false;
 		
-		for (int i = 2; i <= Math.sqrt(n); i++) {
+		for (int i = 2; i <= n - 1; i++) {
 			if (n % i == 0) return false;
 		}
 		

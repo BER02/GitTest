8694d075b9362f5a9308b47d74e494d73c8056c3
S <s>
2023-11-06 12:09:05 +0400

    {"types":"C","runs":7}

--- Problem26.java
+++ Problem26.java
@@ -1,22 +1,35 @@
 import acm.program.ConsoleProgram;
 
-public class Problem26 extends ConsoleProgram {
+public class Problem26 extends ConsoleProgram{
 	public void run() {
-		int n = readInt("Enter n: " );
-		int m = readInt("Enter m: ");
-		
-		println(gcd(n, m));
+		int n = readPositiveInt("Enter n: ");
+		int m = readPositiveInt("Enter m: ");
+	
+		int gcd = gcd(n, m);
+		println("GCD is: " + gcd);
+		int lcm = lcm(n, m);
+		println("LCM is: " + lcm);
 	}
 
-	private int gcd(int x, int y) {
-		int gcd = 0;
-		for (int i = Math.min(x, y); i >= 1; i--) {
-			if(x % i == 0 && y % i == 1) {
-				gcd = i;
-				break;
+	private int readPositiveInt(String text) {
+		int n = readInt(text);
+		while( n <= 0) {
+			println("Number should positive");
+			n = readInt(text);
+		}
+		return n;
+	}
+	
+	private int lcm(int n, int m) {
+		return n * m / gcd(n, m);
+	}
+
+	private int gcd(int n, int m) {
+		for (int i = Math.min(n, m); i >= 1; i--) {
+			if(n % i == 0 && m % i == 0) { // && logical and
+				return i;
 			}
-		}	
-		
-		return gcd;
+		}
+		return -1;
 	}
 }

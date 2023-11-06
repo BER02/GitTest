8ae4b2bac55d4a00d865cbc22e2a1855f71dae3b
S <s>
2023-11-06 12:05:41 +0400

    {"types":"C","runs":4}

--- Problem26.java
+++ Problem26.java
@@ -8,9 +8,11 @@ public class Problem26 extends ConsoleProgram {
 		println(GCD(n, m));
 	}
 
-	private int GCD(int x, int y) {
+	private int gcd(int x, int y) {
 		for (int i = Math.min(x, y); i >= 1; i--) {
-			if(x % i == 0 && y % i == 1) return i;
+			if(x % i == 0 && y % i == 1) {
+				return i;
+			}
 		}
 		return -1;
 		

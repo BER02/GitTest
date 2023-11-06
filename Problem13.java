7c91e4c05334e09ef19af2b4639c652b2c6221b6
S <s>
2023-11-06 12:16:24 +0400

    {"types":"C","runs":8}

--- Problem13.java
+++ Problem13.java
@@ -3,25 +3,18 @@ import acm.program.*;
 
 public class Problem13 extends GraphicsProgram{
 	public void run() {
-		drawMidLine();
-		drawCircle(5,5);
+		int n = readInt("Enter n: ");
+		int m = readInt("Enter m: ");
+		println(gcd(n, m));
 	}
-
-	private void drawCircle(int i, int j) {
-		add(new GOval(10, 10, 100, 100));
-		
-	}
-
-	private void drawMidLine() {
-		double x1 = 0;
-		double y1 = (int) (getHeight() / 2 + 10.5);
-		double x2 = (int) (getWidth());
-		double y2 = y1;
-		GLine midLine = new GLine(x1, y1, x2, y2);
-		add(midLine);
-		
-		double y3 = (double) getHeight() / 2;
-		//add(new GLine(x1, y3, x2, y3));
+	
+	private int gcd(int n, int m) {
+		for (int i = Math.min(n, m); i >= 1; i--) {
+			if (n % i == 0 && m % i == 0) {
+				return i;
+			}
+		}
+		return -1;
 	}
 	
 

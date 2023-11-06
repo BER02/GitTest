d67b8f8b8009d82c73618e209a86ab661a2590cd
S <s>
2023-11-06 15:20:09 +0400

    {"types":"C","runs":17}

--- Problem27.java
+++ Problem27.java
@@ -10,7 +10,7 @@ public class Problem27 extends ConsoleProgram {
 			if (n % 2 == 0) numEven++;
 		}
 		
-		println("number of evens =" + numEven);
+		println("number of evens = " + numEven);
 	}
 	
 	private static final int SENTINEL = -1;

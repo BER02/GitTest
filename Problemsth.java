81e72fc298734e14688400f22cefbf2c13a8da99
S <s>
2023-11-15 01:01:36 +0400

    {"types":"C","runs":43}

--- Problemsth.java
+++ Problemsth.java
@@ -1,28 +1,20 @@
 import java.awt.event.MouseEvent;
+
+import acm.graphics.GLine;
 import acm.graphics.GOval;
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class Problemsth extends GraphicsProgram {
 	private static final int RADIUS = 20;
 
 	public void run() {
-		addMouseListeners();
+		GRect rect = new GRect(10, 10);
+		rect.setFilled(true);
+		add(rect, 100, 100);
+		rect.move(3.0 , 3.0);
 	}
 
-	@Override
-	public void mouseDragged(MouseEvent e) {
-		paintCircle(e.getX(), e.getY());
-	}
-	
-	@Override
-	public void mousePressed(MouseEvent e) {
-		paintCircle(e.getX(), e.getY());
-	}
-	
-	private void paintCircle(int centerX, int centerY) {
-		GOval circle = new GOval(2 * RADIUS, 2 * RADIUS);
-		circle.setFilled(true);
-		add(circle, centerX - RADIUS, centerY - RADIUS);
-	}
+
 	
 }

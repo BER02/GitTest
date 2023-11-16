6743acbaea1709e6bcea8a621ffca61447f2bcd1
S <s>
2023-11-16 05:17:37 +0400

    {"types":"C","runs":8}

--- Problem38.java
+++ Problem38.java
@@ -1,4 +1,29 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class Problem38 {
+public class Problem38 extends ConsoleProgram{
+	
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	private static final int MIN_VALUE = 0;
+	private static final int MAX_VALUE = 36;
 
+	public void run() {
+		int balance = 1000;
+		while(balance > 0) {
+			int bet = readInt("Enter your bet: ");
+			int betNumber = readInt("Enter number:" );
+			int rouletteNumber = rgen.nextInt(MIN_VALUE, MAX_VALUE);
+			if(betNumber == rouletteNumber) {
+				balance += bet;
+				println("Roulette number is: " + rouletteNumber);
+				println("You have won: " + bet);
+				println("Balance: " + balance);
+			} else {
+				balance -= bet;
+				println("Roulette number is: " + rouletteNumber);
+				println("You have lost: " + bet);
+				println("Balance: " + balance);
+			}
+		}
+	}
 }

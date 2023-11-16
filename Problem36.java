5e88b82134a4fbcdca6527f93e792713994b801f
S <s>
2023-11-16 04:55:17 +0400

    {"types":"C","runs":5}

--- Problem36.java
+++ Problem36.java
@@ -1,36 +1,35 @@
 import acm.program.ConsoleProgram;
 import acm.util.RandomGenerator;
 
-public class Problem36 extends ConsoleProgram{
-	
-	private static final int NUMBER_OF_EXPERIMENTS = 10000000;
+public class Problem36 extends ConsoleProgram {
+	private static final int NUM_EXPERIMENTS = 1000000;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 
 	public void run() {
-		double totalFlips = 0;
-		
-		for(int i = 0; i < NUMBER_OF_EXPERIMENTS; i++) {
-			totalFlips += holdExperiment();
+		double sumFlips = 0;
+		for (int i = 0; i < NUM_EXPERIMENTS; i++) {
+			int currFlips = holdExperiment(); // result of 1 experiment
+			sumFlips += currFlips;
 		}
-		
-		double avgFlips = totalFlips / NUMBER_OF_EXPERIMENTS;
-		println("Average number of flips is: " + avgFlips);
+		double avgFlips = sumFlips / NUM_EXPERIMENTS;
+		println("Avg number of flips is: " + avgFlips);
 	}
-	
-	
+
 	private int holdExperiment() {
 		int numFlips = 0;
-		int consecutiveHeads = 0;
-		
-		while(true) {
-			numFlips++;
-			boolean isHead = rgen.nextBoolean();
-			if(isHead) {
-				consecutiveHeads++;
-				if(consecutiveHeads == 3) break; 
-			} else consecutiveHeads = 0;
+		int numConsecutiveHeads = 0;
+		while (true) {
+			numFlips += 1;
+			boolean isHeads = rgen.nextBoolean(0.4);
+			if (isHeads) {
+				numConsecutiveHeads += 1;
+				if (numConsecutiveHeads == 3) {
+					break;
+				}
+			} else {
+				numConsecutiveHeads = 0;
+			}
 		}
-		
 		return numFlips;
 	}
 }

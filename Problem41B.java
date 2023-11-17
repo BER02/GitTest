22af2381b7244e0acd5c7d54988e90116573624e
S <s>
2023-11-17 21:47:59 +0400

    {"types":"C","runs":1}

--- Problem41B.java
+++ Problem41B.java
@@ -1,4 +1,30 @@
+import java.awt.event.MouseEvent;
 
-public class Problem41B {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
+public class Problem41B extends GraphicsProgram{
+	private static final int RADIUS = 20;
+	GOval circle;
+	public void run() {
+		circle = new GOval(2 * RADIUS, 2 * RADIUS);
+		addMouseListeners(); 
+	}
+	
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		paintCircle(e.getX(), e.getY());
+	}
+	
+	@Override
+	public void mousePressed(MouseEvent e) {
+		paintCircle(e.getX(), e.getY());
+	}
+	
+	private void paintCircle(int centerX, int centerY) {
+		circle.setFilled(true);
+		int x = centerX - RADIUS;
+		int y = centerY - RADIUS;
+		add(circle, x, y);
+	}
 }

1b0d8026c4e78e928c2613baa52dc0ec909e6fde
S <s>
2023-11-17 21:00:47 +0400

    {"types":"C","runs":4}

--- Problem40.java
+++ Problem40.java
@@ -8,26 +8,22 @@ public class Problem40 extends GraphicsProgram{
 	private final static int PAUSE = 20;
 	
 	public void run() {
-		addCircle();
-		circleMovement();
-	}
-	
-	private void addCircle() {
-		circle.setSize(2 * BALL_RADIUS, 2 * BALL_RADIUS);
-		circle.setFilled(true);
-		add(circle, 0, getHeight() / 2 - BALL_RADIUS);
-	}
-	
-	private void circleMovement() {
+		GOval circle = createCircle();
 		int vx = VEL_X;
+		
 		while(true) {
-			if(circle.getX() + 2 * BALL_RADIUS >= getWidth() || circle.getX() <= 0) vx = -vx;
-			circle.move(VEL_X, 0);
+			if(circle.getX() <= 0 || circle.getX() + 2 * BALL_RADIUS >= getWidth()) vx = -vx;
+			
+			circle.move(vx, 0);
 			pause(PAUSE);
 		}
-
+		
 	}
 	
-	private GOval circle;
-
+	private GOval createCircle() {
+		GOval circle = new GOval(2 * BALL_RADIUS, 2 * BALL_RADIUS);
+		circle.setFilled(true);
+		add(circle, 0, getHeight() / 2 - BALL_RADIUS);
+		return circle;
+	}
 }

6b250fdfbcf48498f9a318a26c70eb1972c3f185
S <s>
2023-11-23 15:21:19 +0400

    {"types":"C","runs":2}

--- Problem45.java
+++ Problem45.java
@@ -1,4 +1,17 @@
+import acm.program.ConsoleProgram;
 
-public class Problem45 {
+public class Problem45 extends ConsoleProgram{
+	public void run() {
+		String text = readLine("Enter the text: ");
+		char symbol = readLine("Enter the symbol: ").charAt(0);
+		
+		int answer = 0;
+		
+		for(int i = 0; i < text.length(); i++) {
+			if(text.charAt(i) == symbol) answer++;
+		}
+		
+		println("Number of symbol in text: " + answer);
+	}
 
 }

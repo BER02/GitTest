a8fa6f8594faf3ac50447ccb34410231d129cfa6
S <s>
2023-11-23 15:36:38 +0400

    {"types":"C","runs":11}

--- Problem45.java
+++ Problem45.java
@@ -15,3 +15,23 @@ public class Problem45 extends ConsoleProgram{
 	}
 
 }
+
+//public class Problem45 extends ConsoleProgram{
+//
+//	public void run() {
+//		String text = readLine("enter text: ");
+//		char symbol = readLine("enter symbol: ").charAt(0);
+
+//		println("Count: " + countSymbol(text, symbol));
+//	}
+//	
+//	private int countSymbol(String text, char symbol) {
+//		int symbolCount = 0;
+//		for(int  i = 0; i < text.length(); i++) {
+//			if(text.charAt(i) == symbol) {
+//				symbolCount++; // symbolCount = symbolCount + 1;
+//			}
+//		}
+//		return symbolCount;
+//	}
+//}
--- Problem47.java
+++ Problem47.java
@@ -1,4 +1,18 @@
+import acm.program.ConsoleProgram;
 
-public class Problem47 {
+public class Problem47 extends ConsoleProgram{
+	public void run() {
+		String text = readLine("Enter text: ");
+		println(reverse(text));
+	}
+	
+	private String reverse(String text) {
+		String reversedText = "";
+		for(int i = text.length() - 1; i >= 0; i--) {
+			reversedText += text.charAt(i);
+		}
+		
+		return reversedText;
+	}
 
 }

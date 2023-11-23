8f3a9c646b5a792487adcd09781d9084e6ee15db
S <s>
2023-11-23 16:17:29 +0400

    {"types":"C","runs":18}

--- Problem47.java
+++ Problem47.java
@@ -15,5 +15,47 @@ public class Problem47 extends ConsoleProgram{
 		
 		return reversedText;
 	}
-
 }
+
+//public class Problem47 extends ConsoleProgram{
+//	public void run() {
+//		while(true){
+//			String str = readLine("Enter String: ");
+//			if(isPalindrome(str)){
+//				println("Is a palindrome"); 
+//			}else{
+//				println("Is not a palindrome"); 
+//			}
+//		}
+//	}
+//
+//   //	abba
+//   //	i = 0; i = 1 +
+//   //	aba
+//   //	i = 0;  + //i = 1 ??
+//	
+//   //	i = 0, j = 4 - 1 - 0 = 3
+//   //	i = 1, j = 4 - 1 - 1 = 2
+//	private boolean isPalindrome(String text) {
+//		for(int i = 0; i < text.length() / 2; i++) {
+//			if(text.charAt(i) != text.charAt(text.length() - 1 - i)) {
+//				return false;
+//			}
+//		}
+//		return true;
+//	}
+//	
+//	// second version using reverse string
+//	private boolean isPalindrome2(String str){
+//		return str.equals(reversedString(str));
+//	}
+//	
+//	private String reversedString(String str) {
+//		String revStr = "";
+//		for(int i = str.length() - 1; i >= 0; i--){
+//			revStr += str.charAt(i);
+//		}
+//		return revStr;
+//	}
+//
+//}
--- Problem48.java
+++ Problem48.java
@@ -1,4 +1,37 @@
+import acm.program.ConsoleProgram;
 
-public class Problem48 {
+public class Problem48 extends ConsoleProgram{
+	public void run() {
+		String text = readLine("Enter text: ");
+		charStatistics(text);
+	}
+	
+	private void charStatistics(String text) {
+		for(int i = 0; i < text.length(); i++) {
+			if(notChecked(text, i)) {
+				println(text.charAt(i) + ": " + charCount(text, text.charAt(i)));
+			}
+		}
+			
+	}
+	
+	private int charCount(String text, char ch) {
+		int answer = 0;
+		for(int i = 0; i < text.length(); i++) {
+			if(text.charAt(i) == ch) answer++;
+		}
+		
+		return answer;
+	}
+	
+	private boolean notChecked(String text, int n) {
+		char currChar = text.charAt(n);
+		for(int i = 0; i < n; i++) {
+			if(text.charAt(i) == currChar) return false;
+		}
+		
+		return true;
+	}
+	
 
 }

cdd12103565e1e017a3999eb02c3ac49fdb449b6
S <s>
2023-11-23 15:39:37 +0400

    {"types":"C","runs":16}

--- Problem47.java
+++ Problem47.java
@@ -3,8 +3,8 @@ import acm.program.ConsoleProgram;
 public class Problem47 extends ConsoleProgram{
 	public void run() {
 		String text = readLine("Enter text: ");
-		if(text.equals(reverse(text))) println("Is polindrome.");
-		else println("Isn't polindrome.");
+		if(text.equals(reverse(text))) println("Is polindrome");
+		else println("Isn't polindrome");
 	}
 	
 	private String reverse(String text) {

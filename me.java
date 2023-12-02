b88a623ba801283a244079e4c604ba7e263b5f8f
S <s>
2023-12-02 14:17:22 +0400

    {"types":"C","runs":1}

--- me.java
+++ me.java
@@ -1,26 +1,8 @@
-import java.awt.event.MouseEvent;
+import acm.program.ConsoleProgram;
 
-import acm.graphics.GLine;
-import acm.graphics.GPoint;
-import acm.program.GraphicsProgram;
-
-public class me extends GraphicsProgram{
-	private GPoint prevPoint;
-	private GLine line;
-	public void run(){
-		addMouseListeners();
-		prevPoint = new GPoint(0, 0);
-	}
-	
-	public void mousePressed(MouseEvent e){
-		line = new GLine(prevPoint.getX(), prevPoint.getY(),
-				e.getX(), e.getY());
-		add(line);
-		prevPoint = new GPoint(e.getX(), e.getY());
-	}
-	
-	public void mouseDragged(MouseEvent e){
-		line.setEndPoint(e.getX(), e.getY());
-		prevPoint = new GPoint(e.getX(), e.getY());
+public class me extends ConsoleProgram{
+	public void run() {
+	int n = 'C' - 1;
+	println(n);
 	}
 }

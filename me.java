83896826390b19482f11b1848c7b15f9b25f12ff
S <s>
2023-12-06 01:49:08 +0400

    {"types":"C","runs":1}

--- me.java
+++ me.java
@@ -1,36 +1,14 @@
-import java.util.Arrays;
-import acm.program.ConsoleProgram;
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
-public class me extends ConsoleProgram{
+public class me extends GraphicsProgram{
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	
 	public void run() {
-		int n = readInt("Enter n: ");
-		int[] arr = new int[n];
-		for(int i = 0; i < n; i++) {
-			arr[i] = readInt("Enter number: ");
-		}
-		println(Arrays.toString(arr));
-		println(isSorted2(arr));
-		println(isSorted(arr));
-	}
-	
-	boolean isSorted(int[] arr) {
-		for(int i = 1; i < arr.length; i++) {
-			if(arr[i] < arr[i - 1]) {
-				return false;
-			}
-		}
-		return true;
-	}
-	
-	boolean isSorted2(int[] arr) {
-		int[] arrCopy = Arrays.copyOf(arr, arr.length); // instead of manually copying
-//		int[] arrCopy = new int[arr.length];
-//		for (int i = 0; i < arr.length; i++) {
-//			arrCopy[i] = arr[i];
-//		}
-//		
-		Arrays.sort(arrCopy);
-//		Arrays.sort(arr); // bad style. ! DO NOT modify arguments if it is not specified
-		return Arrays.equals(arr, arrCopy);
+		GOval circle = new GOval(100, 100);
+		circle.setFilled(true);
+		circle.setColor(rgen.nextColor());
+		add(circle, 100, 100);
 	}
 }

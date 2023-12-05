3a274cf30e02e73c2b7308922b427599e6784392
S <s>
2023-12-06 00:28:13 +0400

    {"types":"C","runs":12}

--- Problem54.java
+++ Problem54.java
@@ -7,6 +7,7 @@ public class Problem54 extends ConsoleProgram{
 		for(int i = 0; i < n; i++) {
 			arr[i] = readInt("Enter number: ");
 		}
+		
 		int biggest1 = arr[0];
 		int biggest2 = arr[0];
 		for(int i = 0; i < arr.length; i++) {

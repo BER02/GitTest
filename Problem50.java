443b6ce59b558cda25cc2cae87b77c2b1b7a4289
S <s>
2023-12-05 17:59:49 +0400

    {"types":"C","runs":1}

--- Problem50.java
+++ Problem50.java
@@ -1,4 +1,16 @@
+import java.util.StringTokenizer;
+import acm.program.ConsoleProgram;
 
-public class Problem50 {
+public class Problem50 extends ConsoleProgram{
+	public void run() {
+		int numOfWords = 0;
+		String text = readLine("Enter text: ");
+		StringTokenizer tokenizer = new StringTokenizer(text);
+		while(tokenizer.hasMoreTokens()) {
+			numOfWords++;
+		}
+		
+		println("Number of words: " + numOfWords);
+	}
 
 }

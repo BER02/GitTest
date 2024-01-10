a1372637905e4651f8dba30ef0027d73f98851da
S <s>
2024-01-10 09:59:14 +0400

    {"types":"C","runs":5}

--- Problem58.java
+++ Problem58.java
@@ -5,7 +5,7 @@ public class Problem58 extends ConsoleProgram{
 		int[][] matrix = new int[3][3];
 		for(int i = 0; i < 3; i ++) {
 			for(int j = 0; j < 3; j++) {
-				matrix[i][j] = i * 3 + j;
+				matrix[i][j] = i * 3 + j + 1;
 			}
 		}
 		

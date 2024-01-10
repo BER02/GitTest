0db4795d4026c6fa7903fd492098d39c571da68e
S <s>
2024-01-10 13:04:22 +0400

    {"types":"C","runs":2}

--- Problem61.java
+++ Problem61.java
@@ -8,8 +8,8 @@ public class Problem61 extends ConsoleProgram{
 		for(int i = 0; i < n; i++) {
 			nNumbers[i] = readInt("Enter number: ");
 		}
-		int num1;
-		int num2;
+		int num1 = 0;
+		int num2 = 0;
 		for(int i = 0; i < n - 1; i++) {
 			for(int j = i + 1; j < n; j++) {
 				if(nNumbers[i] + nNumbers[j] == m) {
@@ -19,7 +19,7 @@ public class Problem61 extends ConsoleProgram{
 				}
 			}
 		}
-		println("Numbers are " + n + " and " + m);
+		println("Numbers are " + num1 + " and " + num2);
 	}
 
 }

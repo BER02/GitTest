d77857352d441b1f319ddad33831f30164261b41
S <s>
2024-01-10 13:03:27 +0400

    {"types":"C","runs":1}

--- Problem61.java
+++ Problem61.java
@@ -1,4 +1,25 @@
+import acm.program.ConsoleProgram;
 
-public class Problem61 {
+public class Problem61 extends ConsoleProgram{
+	public void run() {
+		int m = readInt("Enter m: ");
+		int n = readInt("Enter n: ");
+		int[] nNumbers = new int[n];
+		for(int i = 0; i < n; i++) {
+			nNumbers[i] = readInt("Enter number: ");
+		}
+		int num1;
+		int num2;
+		for(int i = 0; i < n - 1; i++) {
+			for(int j = i + 1; j < n; j++) {
+				if(nNumbers[i] + nNumbers[j] == m) {
+					num1 = nNumbers[i];
+					num2 = nNumbers[j];
+					break;
+				}
+			}
+		}
+		println("Numbers are " + n + " and " + m);
+	}
 
 }

af7f52dde191eaa30595afc2ed8669e1d49803cc
S <s>
2024-01-10 10:04:20 +0400

    {"types":"C","runs":7}

--- Problem58.java
+++ Problem58.java
@@ -1,17 +1,15 @@
 import acm.program.ConsoleProgram;
 
 public class Problem58 extends ConsoleProgram{
+
 	public void run() {
-		int[][] matrix = new int[3][4];
-		for(int i = 0; i < 3; i ++) {
-			for(int j = 0; j < 3; j++) {
-				matrix[i][j] = i * 3 + j + 1;
-			}
-		}
-		
-		println(magicMatrix(matrix));
+		int[][] matrix1 = {{1, 2, 3}, {6, 9, 8}, {5, 4, 7}};
+		int[][] matrix2 = {{1, 2, 3, 4, 5}, {5, 6, 7, 8, 9}};
+		int[][] matrix3 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 1}};
+		println(magicMatrix(matrix1));
+		println(magicMatrix(matrix2));
+		println(magicMatrix(matrix3));
 	}
-	
 	private boolean magicMatrix(int[][] matrix) {
 		if(matrix.length == matrix[0].length && containsAllNumbers(matrix)) return true;
 		return false;

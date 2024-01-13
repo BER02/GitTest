c36834beb79d1199071e94b1ef8439f312b6b2cd
S <s>
2024-01-13 22:55:53 +0400

    {"types":"C","runs":7}

--- Problem62.java
+++ Problem62.java
@@ -1,4 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
 
-public class Problem62 {
+import acm.program.ConsoleProgram;
 
+public class Problem62 extends ConsoleProgram{
+	private static final String SENTINEL = "";
+	
+	public void run() {
+		Map<String, Integer> nameCount = new HashMap<String, Integer>();
+		while(true) {
+			String name = readLine("Enter name: ");
+			if(name.equals(SENTINEL)) break;
+			
+			if(nameCount.get(name) == null) {
+				nameCount.put(name, 0);
+			}
+			
+			int currCount = nameCount.get(name);
+			nameCount.put(name, currCount + 1);
+			
+		}
+		
+		println(nameCount);
+	}
 }

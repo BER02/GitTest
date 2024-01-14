d3b4717552456d53125712d5ea016ec626443603
S <s>
2024-01-14 19:12:40 +0400

    {"types":"C","runs":0}

--- me.java
+++ me.java
@@ -1,15 +1,7 @@
 import java.util.Arrays;
-
-import acm.util*;
-
-import acm.graphics.GOval;
 import acm.program.ConsoleProgram;
-import acm.program.GraphicsProgram;
-import acm.util.RandomGenerator;
 
-public class me extends ConsoleProgram{
-	private RandomGenerator rgen = RandomGenerator.getInstance();
-	
+public class me extends ConsoleProgram{	
 	public void run() {
 		int[] arr = {3, -2, 3, 6, 1};
 		int[] sortedArr = arr;

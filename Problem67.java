cad515663739280be8e46b332720ef864d2062be
S <s>
2024-01-20 17:44:44 +0400

    {"types":"C","runs":1}

--- Problem67.java
+++ Problem67.java
@@ -1,4 +1,45 @@
+import java.awt.event.ActionEvent;
 
-public class Problem67 {
+import javax.swing.JTextField;
+
+import acm.graphics.GImage;
+import acm.graphics.GLabel;
+import acm.program.GraphicsProgram;
+
+public class Problem67 extends GraphicsProgram{
+	private JTextField imageName;
+	
+	public void init() {
+		imageName = new JTextField();
+		add(imageName, SOUTH);
+		
+		imageName.addActionListener(this);
+		addActionListeners();
+		
+		imageName.setActionCommand("this");
+		
+		GImage image = new GImage("bad path");
+		double x = getWidth()/ 2 - image.getWidth() / 2;
+		double y = getHeight()/ 2 - image.getHeight() / 2;
+		add(image, x, y);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if(e.getActionCommand().equals("this")) {
+			try {
+				GImage image = new GImage(imageName.getText());
+				double x = getWidth()/ 2 - image.getWidth() / 2;
+				double y = getHeight()/ 2 - image.getHeight() / 2;
+				add(image, x, y);
+			} catch(Exception ex) {
+				removeAll();
+				add(new GLabel("Error occured"), 20, 20);
+			}
+			imageName.setText("");
+		}
+		if(e.getSource() == this) {
+			
+		}
+	}
 
 }

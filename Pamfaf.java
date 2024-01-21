887cc56c389991e8fd7a41590334888c02dae63e
S <s>
2024-01-21 19:37:17 +0400

    {"types":"C","runs":18}

--- Pamfaf.java
+++ Pamfaf.java
@@ -1,4 +1,12 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
-public class Pamfaf {
+public class Pamfaf extends GraphicsProgram{
+	public void run() {
+		GLabel text = new GLabel("me");
+		add(text, 20, 20);
+		text.setLabel("zaza);
+	}
+	
 
 }

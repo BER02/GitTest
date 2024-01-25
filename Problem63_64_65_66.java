ba869f33ee48d76c2c335d70355fcdb1d5e9f8da
S <s>
2024-01-25 18:06:37 +0400

    {"types":"C","runs":5}

--- Problem63_64_65_66.java
+++ Problem63_64_65_66.java
@@ -6,10 +6,9 @@ import acm.program.ConsoleProgram;
 
 public class Problem63_64_65_66 extends ConsoleProgram{
 	private static final String SENTINEL = "";
+	private Map<String, ArrayList<String>> friendsList = new HashMap<String, ArrayList<String>>();
 	
 	public void run() {
-		Map<String, ArrayList<String>> friendsList = new HashMap<String, ArrayList<String>>();
-		
 		while(true) {
 			String friend = readLine("Enter friend name: ");
 			if(friend.equals(SENTINEL)) break;
@@ -26,15 +25,7 @@ public class Problem63_64_65_66 extends ConsoleProgram{
 			}	
 		}
 		
-		int maxFriends = 0;
-		String personWithMostFriends = null;
-		for(String person : friendsList.keySet()) {
-			if(friendsList.get(person).size() > maxFriends) {
-				maxFriends = friendsList.get(person).size();
-				personWithMostFriends = person;
-			}
-		}
-		println("Person with most friends is " + personWithMostFriends);
+		println("Person with most friends is " + getPersonWithMostFriends(friendsList));
 		
 		ArrayList<String> allNames = new ArrayList<String>();
  		for(String person : friendsList.keySet()) {
@@ -50,11 +41,55 @@ public class Problem63_64_65_66 extends ConsoleProgram{
  				String friends = friendsList.get(name).toString();
  				println(name + ": " + friends);
  			}
- 		}
+ 		} 
  		
+ 		println("Person who is friend of most people: " + getPersonWithMostFriends(reverse(friendsList)));
  		
- 		
- 		
+ 		println(twoPeopleWithNoCommonFriends(friendsList));
+	}
+	
+	private String getPersonWithMostFriends(Map<String, ArrayList<String>> map) {
+		int maxFriends = 0;
+		String personWithMostFriends = null;
+		for(String person : map.keySet()) {
+			if(map.get(person).size() > maxFriends) {
+				maxFriends = map.get(person).size();
+				personWithMostFriends = person;
+			}
+		}
+		
+		return personWithMostFriends;
+		}
+		
+	private Map<String, ArrayList<String>> reverse(Map<String, ArrayList<String>> map) {
+		Map<String, ArrayList<String>> reversed = new HashMap<String, ArrayList<String>>();
+		for(String person : map.keySet()) {
+			for(String friend : map.get(person)) {
+				reversed.putIfAbsent(friend, new ArrayList<String>());
+				reversed.get(friend).add(person);
+			}
+		}
+		return reversed;
+	}
+	
+	private boolean haveCommonFriend(ArrayList<String> friends1, ArrayList<String> friends2) {
+		for(String friend : friends1) {
+			if(friends2.contains(friend)) return true;
+		}
+		return false;
+	}
+	
+	private String twoPeopleWithNoCommonFriends(Map<String, ArrayList<String>> map) {
+		for(String person1 : map.keySet()) {
+			for(String person2 : map.keySet()) {
+				if(person1.equals(person2)) continue;
+				ArrayList<String> friends1 = map.get(person1);
+				ArrayList<String> friends2 = map.get(person2);
+				if(!haveCommonFriend(friends1, friends2)) return "Two person with no common friends are " + person1 + " and " + person2;
+			}
+		}
+		
+		return "All of them have at least one friend in common";
 	}
 	
 

aa1cbada5ed5083bd583ecba3acac73cf6c4703e
S <s>
2024-01-25 17:19:27 +0400

    {"types":"C","runs":2}

--- Problem68_69.java
+++ Problem68_69.java
@@ -1,4 +1,72 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
 
-public class Problem68_69 {
+import javax.swing.*;
 
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+
+public class Problem68_69 extends GraphicsProgram{
+	private static final int OFFSET = 50;
+	private JTextField text;
+	private JButton button;
+	private GCompound allMessages;
+	private int x = OFFSET;
+	private int y = OFFSET;
+	private int prevY;
+	
+	public void init() {
+		text = new JTextField(40);
+		add(text, SOUTH);
+		
+		text.addActionListener(this);
+		
+		button = new JButton("Enter");
+		add(button, SOUTH);
+		
+		allMessages = new GCompound();
+		add(allMessages);
+		
+		addActionListeners();
+		addMouseListeners();	
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == text || e.getSource() == button) {
+			String message = text.getText();
+			displayMessage(message);
+			text.setText("");
+		}
+	}
+	
+	private void displayMessage(String message) {
+		if(message == null || message.equals("")) return;
+		GLabel messageLabel = new GLabel(message);
+		allMessages.add(messageLabel, x, y);
+		y += OFFSET;
+		
+		correctAllMessagesPosition();
+	}
+	
+	private void correctAllMessagesPosition() {
+		if(allMessages.getHeight() + OFFSET > getHeight()) {
+			allMessages.setLocation(allMessages.getX(), getHeight() - allMessages.getHeight() - OFFSET);
+		}	
+	}
+	
+	public void mouseClicked(MouseEvent e) {
+		prevY = e.getY();
+	}
+	
+	public void mouseDragged(MouseEvent e) {
+		if(allMessages.getHeight() + OFFSET <= getHeight()) return;
+		allMessages.move(0, e.getY() - prevY);
+		prevY = e.getY();
+		
+		if(allMessages.getY() > 0) {
+			allMessages.setLocation(allMessages.getX(), 0);
+		} else if(allMessages.getY() < getHeight() - allMessages.getHeight() - OFFSET) {
+			allMessages.setLocation(allMessages.getX(), getHeight() - allMessages.getHeight() - OFFSET);
+		}
+	}
 }
--- Problem70.java
+++ Problem70.java
@@ -1,4 +1,51 @@
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 
-public class Problem70 {
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
+
+public class Problem70 extends GraphicsProgram implements ComponentListener{ 
+	private static final int GRID_NUM = 10;
+	
+	public void init() {
+		addComponentListener(this);
+		draw();	
+	}
+	
+	private void draw() {
+		removeAll();
+		double width = (double) getWidth() / GRID_NUM;
+		double height = (double) getHeight() / GRID_NUM;
+		for(int i = 0; i < GRID_NUM; i++) {
+			for(int j = 0; j < GRID_NUM; j++) {
+				GRect rect = new GRect(width, height);
+				add(rect, i * width, j * height);
+			}
+		}
+	}
+
+	@Override
+	public void componentHidden(ComponentEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void componentMoved(ComponentEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void componentResized(ComponentEvent arg0) {
+		draw();
+		
+	}
+
+	@Override
+	public void componentShown(ComponentEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
